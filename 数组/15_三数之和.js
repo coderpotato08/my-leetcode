bb7c2ae3958c68e941a12a4355b36528f5ae70fc
coder potato <670190167@qq.com>
2024-01-07 13:53:26 +0800

    【2024-01-07】哈希表（454）数组（15）

--- 数组/15_三数之和.js
+++ 数组/15_三数之和.js
@@ -1,26 +1,26 @@
 function threeSum(nums) {
-  let arr = nums.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b);
   let k, result = [];
-  for(k = 0; k < arr.length - 2; k++) {
-    if (arr[k] > 0) break;
-    if (k > 0 && arr[k] == arr[k - 1]) continue;
-    let i = k + 1, j = arr.length - 1;
+  for(k = 0; k < nums.length - 3; k++) {
+    if (nums[k] > 0) break;
+    if (k > 0 && nums[k] == nums[k - 1]) continue;
+    let i = k + 1, j = nums.length - 1;
     while(i < j) {
-      let sum = arr[k] + arr[i] + arr[j];
+      let sum = nums[k] + nums[i] + nums[j];
       if (sum < 0) {
         i ++;
       } else if (sum > 0) {
         j --;
       } else {
-        result.push([arr[k], arr[i], arr[j]])
+        result.push([nums[k], nums[i], nums[j]]);
+        while(i < j && nums[i] == nums[i + 1]) {
+          i ++;
+        }
+        while(i < j && nums[j] == nums[j - 1]) {
+          j --;
+        }
         i ++;
         j --;
-      }
-      while(i < j) {
-        if (arr[i] == arr[i - 1]) i ++;
-      }
-      while(i < j) {
-        if (arr[j] == arr[j + 1]) j --;
       }
     }
   }
